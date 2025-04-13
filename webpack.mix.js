7f76ae2ae09f2ba8fc0eccdd5947a29af95b897a
hasrinuryawati <53637311+hasrinuryawati@users.noreply.github.com>
2025-04-14 03:18:21 +0700

    back

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-// webpack.mix.js
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
-
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
